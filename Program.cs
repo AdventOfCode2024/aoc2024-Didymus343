19398b0d4bc074d2c6ff9b31e1f153ba088eebd0
Didymus343 <33725436+Didymus343@users.noreply.github.com>
2024-12-02 16:59:40 +0100

    Add files via upload
    Initial commit with Da01 and half of Day02.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//using AdventOfCode2024._2021.Day01;
+using AdventOfCode2024.Day01;
+using AdventOfCode2024.Day02;
+
+Console.WriteLine("Hello, ready for Advent of Code 2024!");
+
+var day = new Day02().First();
+
+Console.ReadKey();
